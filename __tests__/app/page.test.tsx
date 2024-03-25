33af01ecea98f3763c900f25cfe61f1716690a60
Samuel Bran <hello@samuelbran.com>
2024-03-25 17:44:22 -0600

    ✨ Add and configure Jest

---
+++ __tests__/app/page.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import Page from '../../app/page';
+
+describe('Page', () => {
+  it('renders a path to file', () => {
+    render(<Page />);
+
+    const heading = screen.getByText('app/page.tsx');
+
+    expect(heading).toBeInTheDocument();
+  });
+});
