33eeb7298b9bdc6b75009cf3ee2cd50af4433ef3
Samuel Bran <hello@samuelbran.com>
2024-03-25 18:17:00 -0600

    ✨ Add middleware to handle dashboard routes protection

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { cookies } = request;
+  const { redirect } = NextResponse;
+
+  const Authorization = cookies.get('Authorization')?.value;
+
+  /**
+   * DASHBOARD ROUTES
+   */
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    // If theres is no token, redirect the user to login
+    if (!Authorization) return redirect(new URL('/', request.url));
+  }
+
+  /**
+   * AUTH ROUTES
+   */
+  const authRoutes = ['/'];
+  if (authRoutes.includes(request.nextUrl.pathname)) {
+    // If has token redirect to Dashboard
+    if (Authorization) return redirect(new URL('/dashboard', request.url));
+  }
+}
