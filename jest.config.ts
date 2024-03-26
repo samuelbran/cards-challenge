f7f361415172ffe07d1ad4e43f8888afde2c90ce
Samuel Bran <hello@samuelbran.com>
2024-03-25 19:41:51 -0600

    ✨ Update moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -97,8 +97,7 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/providers/(.*)$': '<rootDir>/providers/$1',
-    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+    '^@/actions/(.*)$': '<rootDir>/actions/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
