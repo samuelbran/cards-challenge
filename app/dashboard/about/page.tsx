5a60ac359c1131384d488dd8f0fa7d78ed7918e6
Samuel Bran <hello@samuelbran.com>
2024-03-26 17:16:51 -0600

    ✨ Add About page content

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -1,3 +1,32 @@
+import { PageLayout } from '@/components/PageLayout';
+import { Divider } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Metadata } from 'next/types';
+
+export const metadata: Metadata = {
+  title: 'About Cards Challenge',
+};
+
 export default async function Page() {
-  return <p>About</p>;
+  return (
+    <PageLayout title="About">
+      <Card>
+        <CardContent>
+          <Typography variant="subtitle2" style={{ marginBottom: 20 }}>
+            Cards Challenge
+          </Typography>
+          <Divider />
+          <br />
+          <Typography>Framework: Next.js 14</Typography>
+          <Typography>UI Library: Material UI 5</Typography>
+          <Typography>Data generation: Faker.js</Typography>
+          <Typography>
+            Author: Samuel Bran &lt;hello@samuelbran.com&gt;
+          </Typography>
+        </CardContent>
+      </Card>
+    </PageLayout>
+  );
 }
