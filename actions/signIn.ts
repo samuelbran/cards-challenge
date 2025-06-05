49ac8bb112be0397666aa78eec7c5b668857de0e
Samuel Bran <hello@samuelbran.com>
2025-06-05 17:08:03 -0600

    add todo

--- actions/signIn.ts
+++ actions/signIn.ts
@@ -7,6 +7,8 @@ export async function signIn(formData: FormData) {
   const email = formData.get('email');
   const password = formData.get('password');
 
+  //TODO: Authenticate user information from API
+
   if (email && password) {
     cookies().set('Authorization', 'abc123');
     redirect('/dashboard');
